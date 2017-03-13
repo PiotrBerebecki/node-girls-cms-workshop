e20da4f981fe8edded94c94b2e4691d961cda5a9
PiotrBerebecki <petebere@gmail.com>
2017-03-13 14:32:26 +0000

    Add handler

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var http = require ('http');
 
-var server = http.createServer();
+var server = http.createServer(handler);
+
+var message = "I'm happy";
+
+function handler(request, response) {
+  response.writeHead(200, { "Content-Type": "text/html"} );
+  response.write(message);
+  response.end();
+}
 
 server.listen(3000,function() {
- console.log('server on port 3000'); 
+  console.log('server on port 3000');
 });
-
