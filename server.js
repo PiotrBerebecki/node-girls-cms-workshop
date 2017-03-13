ee5dd3ef1a5e451e34c338ed9784910fab40af19
PiotrBerebecki <petebere@gmail.com>
2017-03-13 14:58:56 +0000

    Serve assets

--- server.js
+++ server.js
@@ -8,17 +8,40 @@ var message = "I'm happy";
 
 function handler(request, response) {
   var endpoint = request.url;
+
+  var extension = path.extname(request.url).slice(1);
+
   if (endpoint === '/') {
-    response.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
+
     fs.readFile(path.join(__dirname, 'public', 'index.html'), function(err, file) {
       if (err) {
         throw new Error(err);
       }
+      response.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
       response.end(file);
     });
+
+  } else if (extension) {
+
+    var extensionType = {
+      "css": "text/css",
+      "jpg": "image/jpeg"
+    };
+
+    fs.readFile(path.join(__dirname, 'public', endpoint), function(err, file) {
+      if (err) {
+        throw new Error(err);
+      }
+      response.writeHead(200, {
+        'Content-Type': extensionType[extension]
+      });
+      response.end(file);
+    });
+
   } else if (endpoint === '/node') {
+
     response.writeHead(200, {
       "Content-Type": "text/html"
     });
