c3f7e20a140aa65a9fa342208cbf0bd85a16e57f
Akin909 <akin.sowemimo@gmail.com>
2017-03-13 14:47:11 +0000

    Added basic routing to index.html and some dummy endpoints

--- server.js
+++ server.js
@@ -1,15 +1,48 @@
-var http = require ('http');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
 var server = http.createServer(handler);
 
 var message = "I'm happy";
 
 function handler(request, response) {
-  response.writeHead(200, { "Content-Type": "text/html"} );
-  response.write(message);
-  response.end();
+  var endpoint = request.url;
+  if (endpoint === '/') {
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    fs.readFile(path.join(__dirname, 'public', 'index.html'), function(err, file) {
+      if (err) {
+        throw new Error(err);
+      }
+      response.end(file);
+    });
+  } else if (endpoint === '/node') {
+    response.writeHead(200, {
+      "Content-Type": "text/html"
+    });
+    response.write('This is the node page');
+    response.end();
+
+  } else if (endpoint === '/girls') {
+
+    response.writeHead(200, {
+      "Content-Type": "text/html"
+    });
+    response.write('This is the girls page');
+    response.end();
+
+  } else {
+
+    response.writeHead(200, {
+      "Content-Type": "text/html"
+    });
+    response.write('Default');
+    response.end();
+  }
 }
 
-server.listen(3000,function() {
-  console.log('server on port 3000');
+server.listen(4000, function() {
+  console.log('server on port 4000');
 });
